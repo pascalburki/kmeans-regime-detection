6aa8a3d61c87b0089841f71eb4f78c9b952b6eaa
pascalburki <paschiburki01@gmail.com>
2026-07-05 17:12:54 +0200

    Update july5_data_setup.py

--- data.foundation/july5_data_setup.py
+++ data.foundation/july5_data_setup.py
@@ -1,1 +1,13 @@
+import numpy as np
+import pandas as pd
+from sklearn.cluster import KMeans 
+import yfinance as yf
 
+df_SPY = yf.download('SPY', '2018-01-01', '2023-12-31')
+
+close = df_SPY["Close"]
+returns = np.log(close/close.shift(1)).dropna()
+vol_20 = returns.rolling(window=20).std()
+
+df_SPY['returns']=returns
+df_SPY['vol_20']=vol_20
